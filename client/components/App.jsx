eedc6623ffa4b42e7d512a4ecb7777f753280963
Kay Christensen <kaychristensen@gmail.com>
2017-09-01 16:17:24 -0700

    Turn App into class component.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import RepoList from './RepoList';
 
-const App = () => (
-  <div>This is the App component</div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      repoList: []
+    };
+  }
+
+  searchRepos(repos) {
+    // call GitHub API
+    // set state to results of search
+  }
+
+  componentDidMount() {
+    // call searchRepos
+  }
+
+  render() {
+    return (
+      <div className='repo-list'>
+        <RepoList repos={this.state.repoList}/>
+      </div>
+    );
+  }
+};
 
 export default App;
