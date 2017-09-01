9561b45a823b3bb236a2720cddebbf454bb46688
Kay Christensen <kaychristensen@gmail.com>
2017-09-01 13:09:07 -0700

    Add express.static for html page.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.set('env', process.env.NODE_ENV);
 app.set('PORT', process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('PORT'), (err) => {
   if (err) {
