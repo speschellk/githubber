61d1c35903e2cc13e2ba342d8b6f892145b05131
Kay Christensen <kaychristensen@gmail.com>
2017-09-06 14:42:34 -0700

    Add client id and secret params.

---
+++ client/config/github.config.js
@@ -0,0 +1,8 @@
+// normally would .gitignore
+
+const credentials = {
+  CLIENT_ID: 'Iv1.6f574182680ab222',
+  CLIENT_SECRET: 'cc714977ada6d91eda1dab5563fcb7e069b0b2e6'
+};
+
+export default credentials;
