2aa6454878aa302ce04b0c08ac61068833b26608
Kay Christensen <kaychristensen@gmail.com>
2017-09-06 16:37:34 -0700

    Add credentials to search.

--- client/lib/searchGitHub.js
+++ client/lib/searchGitHub.js
@@ -1,24 +1,31 @@
 import request from 'superagent';
+import credentials from '../config/github.config'
 
 const searchGitHub = (query = 'stars:>=500', callback) => {
   request.get('https://api.github.com/search/repositories')
+    .auth(credentials.username, credentials.password)
     .query({
-      q:query,
-      sort:'stars',
-      order:'desc',
-      per_page:'100',
-      page:'1'
+      q: query,
+      sort: 'stars',
+      order: 'desc',
+      per_page: '100',
+      page: '1'
     })
     .then(({ body: { items } }) => {
       items.forEach((item) => {
-        request.get(item.contributors_url)
-          .then(({ body }) => {
-            item.contributor = body[0].login;
-            item.contributions = body[0].contributions;
-          })
-          .then(() => {
-            callback && callback(items);
-          });
+        if (!item.contributor) {
+          request.get(item.contributors_url)
+            .then(({ body }) => {
+              item.contributor = body[0].login;
+              item.contributions = body[0].contributions;
+            })
+            .then(() => {
+              callback && callback(items);
+            })
+            .catch((err) => {
+              console.log('Error getting contributors:', err);
+            });
+        }
       })
     })
     .catch((err) => {
