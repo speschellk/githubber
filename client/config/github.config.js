ec9de047ffa5c9724448301dc4e9f7a1b23b69bc
Kay Christensen <kaychristensen@gmail.com>
2017-09-06 16:40:58 -0700

    Remove config.

--- client/config/github.config.js
+++
@@ -1,8 +0,0 @@
-// normally would .gitignore
-
-const credentials = {
-  CLIENT_ID: 'Iv1.6f574182680ab222',
-  CLIENT_SECRET: 'cc714977ada6d91eda1dab5563fcb7e069b0b2e6'
-};
-
-export default credentials;
