a25636969b263923db5840e17231331da9defd76
Kay Christensen <kaychristensen@gmail.com>
2017-09-06 16:23:07 -0700

    Rename to example config.

---
+++ client/config/github.config.example.js
@@ -0,0 +1,8 @@
+// Make a copy called 'github.config.js', which will be gitignored
+
+const credentials = {
+  username: '<your GitHub username>',
+  password: '<your GitHub password>'
+};
+
+export default credentials;
